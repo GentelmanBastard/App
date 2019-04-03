6bc604048ba6a8a711789f149c5693eb240f0fde
mkucman <mkucman@hotmail.com>
2019-04-03 23:17:13 +0200

    Cleaned up code.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,5 @@
 package sample;
 
-import java.io.InputStream;
-
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -10,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.InputStream;
 
 public class Controller {
     @FXML
@@ -20,54 +19,37 @@ public class Controller {
     private TextField pass;
     @FXML
     private TextField boxIp;
-    public void onButtonClicked(){
-        String host=boxIp.getText();
-        String user=username.getText();
-        String password=pass.getText();
-        String command1="ls -ltr";
-        try{
 
+    public void onButtonClicked() {
+        String host = boxIp.getText();
+        String user = username.getText();
+        String password = pass.getText();
+        String command1 = "ls -ltr";
+        try {
             java.util.Properties config = new java.util.Properties();
             config.put("StrictHostKeyChecking", "no");
             JSch jsch = new JSch();
-            Session session=jsch.getSession(user, host, 22);
+            Session session = jsch.getSession(user, host, 22);
             session.setPassword(password);
             session.setConfig(config);
             session.setConfig("PreferredAuthentications",
                     "publickey,keyboard-interactive,password");
             session.connect();
-            log.appendText("Connected");
-//            System.out.println("Connected");
-
-            Channel channel=session.openChannel("exec");
+            log.appendText("Connected\n");
+            Channel channel = session.openChannel("exec");
             // Rzutowanie elementu channel and klasę ChannelExec i uruchamiamy metodę setCommand
-            ((ChannelExec)channel).setCommand(command1);
+            ((ChannelExec) channel).setCommand(command1);
             channel.setInputStream(null);
-            ((ChannelExec)channel).setErrStream(System.err);
-
-            InputStream in=channel.getInputStream();
+            ((ChannelExec) channel).setErrStream(System.err);
+            InputStream in = channel.getInputStream();
             channel.connect();
-            byte[] tmp=new byte[1024];
-            int i=in.read(tmp, 0, 1024);
+            byte[] tmp = new byte[1024];
+            int i = in.read(tmp, 0, 1024);
             log.appendText(new String(tmp, 0, i));
-//
-//            while(true){
-//                while(in.available()>0){
-//                    int i=in.read(tmp, 0, 1024);
-//                    if(i<0)break;
-//                    System.out.print(new String(tmp, 0, i));
-//                }
-//                if(channel.isClosed()){
-//                    System.out.println("exit-status: "+channel.getExitStatus());
-//                    break;
-//                }
-//                try{Thread.sleep(1000);}catch(Exception ee){}
-//            }
             channel.disconnect();
             session.disconnect();
-            log.appendText("DONE");
-//            System.out.println("DONE");
-        }catch(Exception e){
+            log.appendText("DONE\n");
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
