27d8210275823bcdea0ca53aa220cbb74886b10c
mkucman <mkucman@hotmail.com>
2019-04-03 22:59:25 +0200

    Changed the logging and added TextArea

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,10 +7,13 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 
 public class Controller {
+    @FXML
+    private TextArea log;
     @FXML
     private TextField username;
     @FXML
@@ -33,7 +36,8 @@ public class Controller {
             session.setConfig("PreferredAuthentications",
                     "publickey,keyboard-interactive,password");
             session.connect();
-            System.out.println("Connected");
+            log.appendText("Connected");
+//            System.out.println("Connected");
 
             Channel channel=session.openChannel("exec");
             // Rzutowanie elementu channel and klasę ChannelExec i uruchamiamy metodę setCommand
@@ -44,21 +48,25 @@ public class Controller {
             InputStream in=channel.getInputStream();
             channel.connect();
             byte[] tmp=new byte[1024];
-            while(true){
-                while(in.available()>0){
-                    int i=in.read(tmp, 0, 1024);
-                    if(i<0)break;
-                    System.out.print(new String(tmp, 0, i));
-                }
-                if(channel.isClosed()){
-                    System.out.println("exit-status: "+channel.getExitStatus());
-                    break;
-                }
-                try{Thread.sleep(1000);}catch(Exception ee){}
-            }
+            int i=in.read(tmp, 0, 1024);
+            log.appendText(new String(tmp, 0, i));
+//
+//            while(true){
+//                while(in.available()>0){
+//                    int i=in.read(tmp, 0, 1024);
+//                    if(i<0)break;
+//                    System.out.print(new String(tmp, 0, i));
+//                }
+//                if(channel.isClosed()){
+//                    System.out.println("exit-status: "+channel.getExitStatus());
+//                    break;
+//                }
+//                try{Thread.sleep(1000);}catch(Exception ee){}
+//            }
             channel.disconnect();
             session.disconnect();
-            System.out.println("DONE");
+            log.appendText("DONE");
+//            System.out.println("DONE");
         }catch(Exception e){
             e.printStackTrace();
         }
