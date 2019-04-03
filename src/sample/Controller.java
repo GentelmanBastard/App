a77ec6f830f4d8f3b5d96f303aa85db0c4dfa9f7
mkucman <mkucman@hotmail.com>
2019-04-03 23:59:23 +0200

    Added handlers for checkboxes and ifs for options.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,26 +5,37 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.InputStream;
 
 public class Controller {
-    @FXML
-    private TextArea log;
-    @FXML
-    private TextField username;
-    @FXML
-    private TextField pass;
-    @FXML
-    private TextField boxIp;
+    @FXML private TextArea log;
+    @FXML private TextField username;
+    @FXML private TextField pass;
+    @FXML private TextField boxIp;
+    @FXML private RadioButton startover;
+    @FXML private RadioButton processing;
+    @FXML private RadioButton time;
 
     public void onButtonClicked() {
         String host = boxIp.getText();
         String user = username.getText();
         String password = pass.getText();
         String command1 = "ls -ltr";
+        if(startover.isSelected()){
+            command1 = "startover";
+        }
+        else if(processing.isSelected()){
+            command1 = "processing";
+        }
+        else if(time.isSelected()){
+            command1 = "time";
+        }
         try {
             java.util.Properties config = new java.util.Properties();
             config.put("StrictHostKeyChecking", "no");
@@ -36,8 +47,15 @@ public class Controller {
                     "publickey,keyboard-interactive,password");
             session.connect();
             log.appendText("Connected\n");
+            log.appendText("Command executed: " + command1 + "\n");
             Channel channel = session.openChannel("exec");
             // Rzutowanie elementu channel and klasę ChannelExec i uruchamiamy metodę setCommand
+            if(command1.equals("startover") || command1.equals("processing") || command1.equals("time")) {
+                channel.disconnect();
+                session.disconnect();
+                log.appendText("DONE\n");
+                return;
+            }
             ((ChannelExec) channel).setCommand(command1);
             channel.setInputStream(null);
             ((ChannelExec) channel).setErrStream(System.err);
